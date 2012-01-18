2555fed467269516b936fecf00f8119ef0059774
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2012-01-18 18:28:11 +0200

    Input/output streams that supports locking.

---
+++ src/main/java/com/khmelyuk/memory/concurrency/LockInputStream.java
@@ -0,0 +1,54 @@
+package com.khmelyuk.memory.concurrency;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * The input stream that supports locking.
+ *
+ * @author Ruslan Khmelyuk
+ */
+public class LockInputStream extends InputStream {
+
+    private final Lock lock;
+    private final InputStream in;
+
+    public LockInputStream(InputStream in, Lock lock) {
+        this.in = in;
+        this.lock = lock;
+    }
+
+    @Override
+    public int read() throws IOException {
+        try {
+            lock.lock();
+            return in.read();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        try {
+            lock.lock();
+            return in.read(b);
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        try {
+            lock.lock();
+            return in.read(b, off, len);
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+}
