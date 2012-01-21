e57244a125e2f733c46a375fd09b9d4deef61f61
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2012-01-21 18:16:13 +0200

    Quality tests for tables.

--- src/test/java/com/khmelyuk/memory/vm/table/ConcurrencyTablePerformanceTestCase.java
+++ src/test/java/com/khmelyuk/memory/vm/table/ConcurrencyTablePerformanceTestCase.java
@@ -16,7 +16,7 @@ public class ConcurrencyTablePerformanceTestCase {
     static final int THREAD_NUM = 3;
 
     @Test
-    public void testOldLinkedTablePerformance() throws Exception {
+    public void testLinkedTablePerformance() throws Exception {
         testPerformance(new LinkedVirtualMemoryTable(SIZE), 0, THREAD_NUM);
         long total = 0;
         for (int i = 0; i < N; i++) {
@@ -28,7 +28,7 @@ public class ConcurrencyTablePerformanceTestCase {
 
 
     @Test
-    public void testLinkedTablePerformance() throws Exception {
+    public void testQueueTablePerformance() throws Exception {
         testPerformance(new QueueVirtualMemoryTable(SIZE), 0, THREAD_NUM);
         long total = 0;
         for (int i = 0; i < N; i++) {
@@ -38,8 +38,65 @@ public class ConcurrencyTablePerformanceTestCase {
         System.out.println("Queue: Avg. duration " + (total / N) + "ms");
     }
 
+    @Test
+    public void testLinkedTableQuality() throws Exception {
+        testQuality(new LinkedVirtualMemoryTable(SIZE), 0, THREAD_NUM);
+        float total = 0;
+        for (int i = 0; i < N; i++) {
+            total += testQuality(new LinkedVirtualMemoryTable(SIZE), i, THREAD_NUM);
+        }
+
+        System.out.println("Linked: Avg. nulls percentage " + Math.round(total / N) + "%");
+    }
+
+    @Test
+    public void testQueueTableQuality() throws Exception {
+        testQuality(new QueueVirtualMemoryTable(SIZE), 0, THREAD_NUM);
+        float total = 0;
+        for (int i = 0; i < N; i++) {
+            total += testQuality(new QueueVirtualMemoryTable(SIZE), i, THREAD_NUM);
+        }
+
+        System.out.println("Queue: Avg. nulls percentage " + Math.round(total / N) + "%");
+    }
+
 
     private static long testPerformance(final VirtualMemoryTable table, int n, int threadsNum) throws Exception {
+        final int max = COUNT_COEFF * (n + 5);
+        final int avgBlockSize = table.getFreeMemorySize() / max;
+        final Block[] blocks = new Block[max];
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                int nullsCount = 0;
+                for (int i = 0; i < max; i++) {
+                    blocks[i] = table.allocate(avgBlockSize);
+                    if (blocks[i] == null) {
+                        nullsCount++;
+                    }
+                    if (i % 2 == 0) {
+                        free(table, blocks, i - 1);
+                    }
+                }
+            }
+        };
+
+        long begin = System.currentTimeMillis();
+        Thread[] threads = new Thread[threadsNum];
+        for (int i = 0; i < threadsNum; i++) {
+            threads[i] = new Thread(runnable);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threadsNum; i++) {
+            threads[i].join();
+        }
+
+        return (System.currentTimeMillis() - begin);
+    }
+
+    private static float testQuality(final VirtualMemoryTable table, int n, int threadsNum) throws Exception {
         final int max = COUNT_COEFF * (n + 5);
         final int avgBlockSize = table.getFreeMemorySize() / max;
         final Block[] blocks = new Block[max];
@@ -64,7 +121,6 @@ public class ConcurrencyTablePerformanceTestCase {
             }
         };
 
-        long begin = System.currentTimeMillis();
         Thread[] threads = new Thread[threadsNum];
         for (int i = 0; i < threadsNum; i++) {
             threads[i] = new Thread(runnable);
@@ -75,9 +131,7 @@ public class ConcurrencyTablePerformanceTestCase {
             threads[i].join();
         }
 
-        System.out.println(table.getClass().getSimpleName() + ": nulls percentage is " + (nullsPercentTotal[0] / threadsNum) + "%");
-
-        return (System.currentTimeMillis() - begin);
+        return (nullsPercentTotal[0] / threadsNum);
     }
 
     private static void free(VirtualMemoryTable table, Block[] blocks, int n) {
