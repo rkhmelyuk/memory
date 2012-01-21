7cc8a83bb343a92f10deaf9c4b952ad103fc449f
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2012-01-21 18:16:54 +0200

    Storage.

---
+++ src/main/java/com/khmelyuk/memory/vm/storage/ByteArrayStorage.java
@@ -0,0 +1,90 @@
+package com.khmelyuk.memory.vm.storage;
+
+import com.khmelyuk.memory.OutOfBoundException;
+
+/**
+ * A byte array storage.
+ *
+ * @author Ruslan Khmelyuk
+ */
+public final class ByteArrayStorage implements Storage {
+
+    private byte[] data;
+    private int size;
+
+    public ByteArrayStorage(int size) {
+        this.size = size;
+        this.data = new byte[size];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void free() {
+        data = new byte[0];
+        size = 0;
+    }
+
+    public void write(byte[] data) throws OutOfBoundException {
+        if (data.length > this.data.length) {
+            throw new OutOfBoundException();
+        }
+
+        System.arraycopy(data, 0, this.data, 0, data.length);
+    }
+
+    public void write(byte[] data, int offset) throws OutOfBoundException {
+        if (offset >= size || data.length + offset > size) {
+            throw new OutOfBoundException();
+        }
+
+        System.arraycopy(data, 0, this.data, offset, data.length);
+    }
+
+    public void write(byte[] data, int offset, int length) throws OutOfBoundException {
+        if (offset >= size || length + offset > size) {
+            throw new OutOfBoundException();
+        }
+
+        System.arraycopy(data, 0, this.data, offset, length);
+    }
+
+    public int read(byte[] data) {
+        int length = data.length;
+        if (length > size) {
+            length = size;
+        }
+
+        System.arraycopy(this.data, 0, data, 0, length);
+
+        return length;
+    }
+
+    public int read(byte[] data, int offset, int length) {
+        if (data.length < length) {
+            length = data.length;
+        }
+        if (length == 0 || offset + length > size) {
+            return -1;
+        }
+
+        System.arraycopy(this.data, offset, data, 0, length);
+
+        return length;
+    }
+
+    public void write(byte data, int offset) throws OutOfBoundException {
+        if (offset >= size) {
+            throw new OutOfBoundException();
+        }
+        this.data[offset] = data;
+    }
+
+    public byte read(int offset) {
+        if (offset >= size) {
+            return -1;
+        }
+        return this.data[offset];
+    }
+}
---
+++ src/main/java/com/khmelyuk/memory/vm/storage/ByteBufferStorage.java
@@ -0,0 +1,101 @@
+package com.khmelyuk.memory.vm.storage;
+
+import com.khmelyuk.memory.OutOfBoundException;
+
+import java.nio.ByteBuffer;
+
+/**
+ * A byte array storage.
+ *
+ * @author Ruslan Khmelyuk
+ */
+public final class ByteBufferStorage implements Storage {
+
+    private ByteBuffer data;
+    private int size;
+
+    public ByteBufferStorage(ByteBuffer buffer) {
+        this.data = buffer;
+        this.size = buffer.capacity();
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void free() {
+        data = ByteBuffer.allocate(0);
+        size = 0;
+    }
+
+    public void write(byte[] data) throws OutOfBoundException {
+        if (data.length > this.size) {
+            throw new OutOfBoundException();
+        }
+
+        this.data.slice().put(data);
+    }
+
+    public void write(byte[] data, int offset) throws OutOfBoundException {
+        if (offset >= size || data.length + offset > size) {
+            throw new OutOfBoundException();
+        }
+
+        ByteBuffer buf = this.data.slice();
+        buf.position(offset);
+        buf.put(data);
+    }
+
+    public void write(byte[] data, int offset, int length) throws OutOfBoundException {
+        if (offset >= size || length + offset > size) {
+            throw new OutOfBoundException();
+        }
+
+        ByteBuffer buf = this.data.slice();
+        buf.position(offset);
+        buf.put(data, 0, length);
+    }
+
+    public int read(byte[] data) throws OutOfBoundException {
+        int length = data.length;
+        if (length > size) {
+            length = size;
+        }
+
+        this.data.get(data, 0, length);
+
+        return length;
+    }
+
+    public int read(byte[] data, int offset, int length) {
+        if (offset + length > size) {
+            length = size - offset;
+        }
+        if (length == 0) {
+            return -1;
+        }
+
+        ByteBuffer buf = this.data.slice();
+        buf.position(offset);
+        buf.get(data, 0, length);
+
+        return length;
+    }
+
+    public void write(byte data, int offset) throws OutOfBoundException {
+        if (offset >= size) {
+            throw new OutOfBoundException();
+        }
+
+        this.data.put(offset, data);
+    }
+
+    public byte read(int offset) {
+        if (offset >= this.data.capacity()) {
+            return -1;
+        }
+
+        return data.get(offset);
+    }
+}
